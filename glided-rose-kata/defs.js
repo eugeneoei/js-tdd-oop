2f033bd2abb58fb1ee16867d29f66917ff0296b7
eugeneoei <eugeneoei89@gmail.com>
2021-09-17 16:20:49 +0800

    added backstagePassesItems and legendaryItems variables to defs.js which are used in implementation

--- glided-rose-kata/defs.js
+++ glided-rose-kata/defs.js
@@ -1,17 +1,23 @@
 const agedBrie = 'Aged Brie'
+const durian = 'Durian, the King of fruits'
 const sulfuras = 'Sulfuras'
+const soulStone = 'Soul Stone - one of the Infinity Stones'
 
 const backstagePassesItems = [
-	agedBrie
+	agedBrie,
+	durian
 ]
 
 const legendaryItems = [
-	sulfuras
+	sulfuras,
+	soulStone
 ]
 
 module.exports = {
 	agedBrie,
+	durian,
 	sulfuras,
+	soulStone,
 	backstagePassesItems,
 	legendaryItems
 }
