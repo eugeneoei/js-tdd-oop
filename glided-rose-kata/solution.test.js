d0d9093a558b72fb745d3d5524c176a46a273251
eugeneoei <eugeneoei89@gmail.com>
2021-09-17 11:55:50 +0800

    update Item instance creation test case

--- glided-rose-kata/solution.test.js
+++ glided-rose-kata/solution.test.js
@@ -3,10 +3,16 @@ const { Item } = require('./solution')
 describe('Item', () => {
 
 	// instance creation test
-	it('Should have properties "SellIn" and "Quality" on instance creation', () => {
-		const item = new Item('Item One', 10, 10)
-		expect(item.getSellIn()).toEqual(10)
-		expect(item.getQuality()).toEqual(10)
+	it('Should not be able to access "sellIn" and "quality" private fields', () => {
+		const item = new Item('Item One', 1, 1)
+		expect(item.sellIn).not.toBeDefined()
+		expect(item.quality).not.toBeDefined()
 	})
+
+	// it('Should have properties "sellIn" and "quality" on instance creation', () => {
+	// 	const item = new Item('Item One', 10, 10)
+	// 	expect(item.getSellIn()).toEqual(10)
+	// 	expect(item.getQuality()).toEqual(10)
+	// })
 
 })
