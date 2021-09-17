8ca7b751b50bc8af9904a87b32a0be8d99c0e24f
eugeneoei <eugeneoei89@gmail.com>
2021-09-17 13:43:30 +0800

    update reduce quality by half if sellIn is < 0 test case with scenario where item instance is initialised with sellIn as 2 and quality as 10 for robustness

--- glided-rose-kata/solution.test.js
+++ glided-rose-kata/solution.test.js
@@ -25,13 +25,22 @@ describe('Item', () => {
 	})
 
 	it('Should reduce "quality" by half if "sellIn" is < 0', () => {
-		const item = new Item('Item One', 2, 2)
-		item.reduceSellIn()
-		item.reduceQuality()
-		expect(item.quality).toEqual(1)
-		item.reduceSellIn()
-		item.reduceQuality()
-		expect(item.quality).toEqual(0)
+		const itemTwo = new Item('Item Two', 2, 10)
+
+		itemTwo.reduceSellIn()
+		itemTwo.reduceQuality()
+		expect(item.sellIn).toEqual(1)
+		expect(itemTwo.quality).toEqual(9)
+
+		itemTwo.reduceSellIn()
+		itemTwo.reduceQuality()
+		expect(item.sellIn).toEqual(0)
+		expect(itemTwo.quality).toEqual(8)
+		
+		itemTwo.reduceSellIn()
+		itemTwo.reduceQuality()
+		expect(item.sellIn).toEqual(-1)
+		expect(itemTwo.quality).toEqual(4)
 	})
 
 })
