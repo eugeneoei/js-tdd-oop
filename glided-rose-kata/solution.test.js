75af373289f8f5db2d6a0623bc1d73ab0b1bf9eb
eugeneoei <eugeneoei89@gmail.com>
2021-09-17 17:16:33 +0800

    added test case where shop will update all items at the end of the day and each item's respective sellIn and quality fields should be updated according to if each item is a normal item, backstage pass item or legendary item

--- glided-rose-kata/solution.test.js
+++ glided-rose-kata/solution.test.js
@@ -252,7 +252,7 @@ describe('Shop', () => {
 		const shop = new Shop('Shop One', items)
 		expect(shop.name).toEqual('Shop One')
 		expect(Array.isArray(shop.items)).toEqual(true)
-		expect(shop.items.length).toEqual(3)
+		expect(shop.items.length).toEqual(items.length)
 	})
 
 	it('When a Shop is instantiated with only "name" parameter, "items" field should be an empty array', () => {
@@ -262,24 +262,52 @@ describe('Shop', () => {
 		expect(shop.items.length).toEqual(0)
 	})
 
-	// it('Shop should update all it\'s items at the end of the day', () => {
-	// 	const items = [
-	// 		new Item(agedBrie, 12, 40), // 11, 41
-	// 		new Item(agedBrieA, 5, 48), // 3, 50
-	// 		new Item(agedBrieB, 3, 40), // 2, 43
-	// 		new Item(agedBrieC, 1, 28), // 0, 0
-	// 		new Item(durian, 1, 1),
-	// 		new Item(sulfuras, 1, 1),
-	// 		new Item(sulfurasA, 1, 1),
-	// 		new Item(sulfurasB, 1, 1),
-	// 		new Item(sulfurasC, 1, 1),
-	// 		new Item(soulStone, 1, 1), 
-	// 		new Item('Item One', 10, 55) // 9, 50
-	// 		new Item('Item Two', 8, 0) // 8, 0
-	// 		new Item('Item Three', 24, 18) // 23, 17
-	// 	]
-	// 	const shop = new Shop('Shop One', items)
-	// })
+	it('Shop should update all it\'s items at the end of the day', () => {
+		const items = [
+			new Item(agedBrie, 12, 40),
+			new Item(agedBrieA, 5, 48),
+			new Item(agedBrieB, 3, 40),
+			new Item(agedBrieC, 1, 28),
+			new Item(durian, 10, 70),
+			new Item(sulfuras, 8, 77),
+			new Item(sulfurasA, 20, 55),
+			new Item(sulfurasB, 5, 88),
+			new Item(sulfurasC, 45, 20),
+			new Item(soulStone, 3, 6),
+			new Item('Item One', 10, 55),
+			new Item('Item Two', 8, 0),
+			new Item('Item Three', 24, 18),
+			new Item('Item Four', 0, 18)
+		]
+		const shop = new Shop('Shop One', items)
+
+		shop.updateItemsQuality()
+
+		expectedValues = [
+			{ sellIn: 11, quality: 41 },
+			{ sellIn: 3, quality: 50 },
+			{ sellIn: 2, quality: 43 },
+			{ sellIn: 0, quality: 0 },
+			{ sellIn: 9, quality: 50 },
+			{ sellIn: 7, quality: 80 },
+			{ sellIn: 19, quality: 80 },
+			{ sellIn: 4, quality: 80 },
+			{ sellIn: 44, quality: 80 },
+			{ sellIn: 2, quality: 80 },
+			{ sellIn: 9, quality: 50 },
+			{ sellIn: 7, quality: 0 },
+			{ sellIn: 23, quality: 17 },
+			{ sellIn: -1, quality: 9 }
+		]
+
+		const shopItems = shop.items
+		shop.items.forEach((item, index) => {
+			// console.log('')
+			// console.log(`Test Case for ${item.name}`)
+			expect(item.sellIn).toEqual(expectedValues[index]['sellIn'])
+			expect(item.quality).toEqual(expectedValues[index]['quality'])
+		})
+	})
 
 
 })
