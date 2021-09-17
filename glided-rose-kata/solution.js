090e8e0963e68ba8ef9cf2bb4fea1d543f847617
eugeneoei <eugeneoei89@gmail.com>
2021-09-17 14:33:24 +0800

    update updateQuality function where if aged brie's sellIn is <= 0, quality is 0

--- glided-rose-kata/solution.js
+++ glided-rose-kata/solution.js
@@ -14,7 +14,9 @@ class Item {
 
 		if (this.name === 'Aged Brie') {
 
-			if (this.sellIn <= 5) {
+			if (this.sellIn <= 0) {
+				this.quality = 0
+			} else if (this.sellIn <= 5) {
 				this.quality += 3
 			} else if (this.sellIn <= 10) {
 				this.quality += 2
