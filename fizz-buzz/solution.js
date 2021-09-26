61566365115b13a52baf04d85e43cb9713e75951
eugeneoei <eugeneoei89@gmail.com>
2021-09-26 20:25:09 +0800

    update implmentation where if at each iteration 'i' is a multiple of 5, Buzz is printed

--- fizz-buzz/solution.js
+++ fizz-buzz/solution.js
@@ -2,6 +2,8 @@ const solution = num => {
 	for (let i = 1; i < num + 1; i++) {
 		if (i % 3 === 0) {
 			console.log('Fizz')
+		} else if (i % 5 === 0) {
+			console.log('Buzz')
 		} else {
 			console.log(i)
 		}
