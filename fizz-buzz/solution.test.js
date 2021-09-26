671594a3252af6cb3c470a30fb5a722ea171a04b
eugeneoei <eugeneoei89@gmail.com>
2021-09-26 20:21:55 +0800

    - added test case if number is 5 - refine all test cases with "num" variable as in arrange section of each unit test

--- fizz-buzz/solution.test.js
+++ fizz-buzz/solution.test.js
@@ -1,31 +1,34 @@
 const solution = require('./solution')
 
 it('Should print "1" if number is 1', () => {
+	const num = 1
 	console.log = jest.fn()
 
-	solution(1)
+	solution(num)
 	
 	expect(console.log).toHaveBeenCalledWith(1)
 	expect(console.log).toHaveBeenNthCalledWith(1, 1)
-	expect(console.log).toHaveBeenCalledTimes(1)
+	expect(console.log).toHaveBeenCalledTimes(num)
 })
 
 it('Should print "1" and "2" if number is 2', () => {
+	const num = 2
 	console.log = jest.fn()
 
-	solution(2)
+	solution(num)
 	
 	expect(console.log).toHaveBeenCalledWith(1)
 	expect(console.log).toHaveBeenCalledWith(2)
 	expect(console.log).toHaveBeenNthCalledWith(1, 1)
 	expect(console.log).toHaveBeenNthCalledWith(2, 2)
-	expect(console.log).toHaveBeenCalledTimes(2)
+	expect(console.log).toHaveBeenCalledTimes(num)
 })
 
 it('Should print "1", "2" and "Fizz" if number is 3', () => {
+	const num = 3
 	console.log = jest.fn()
 
-	solution(3)
+	solution(num)
 	
 	expect(console.log).toHaveBeenCalledWith(1)
 	expect(console.log).toHaveBeenCalledWith(2)
@@ -33,7 +36,26 @@ it('Should print "1", "2" and "Fizz" if number is 3', () => {
 	expect(console.log).toHaveBeenNthCalledWith(1, 1)
 	expect(console.log).toHaveBeenNthCalledWith(2, 2)
 	expect(console.log).toHaveBeenNthCalledWith(3, 'Fizz')
-	expect(console.log).toHaveBeenCalledTimes(3)
+	expect(console.log).toHaveBeenCalledTimes(num)
+})
+
+it('Should print "1", "2", "Fizz", "4", "Buzz" if number is 5', () => {
+	const num = 5
+	console.log = jest.fn()
+
+	solution(num)
+	
+	expect(console.log).toHaveBeenCalledWith(1)
+	expect(console.log).toHaveBeenCalledWith(2)
+	expect(console.log).toHaveBeenCalledWith('Fizz')
+	expect(console.log).toHaveBeenCalledWith(4)
+	expect(console.log).toHaveBeenCalledWith('Buzz')
+	expect(console.log).toHaveBeenNthCalledWith(1, 1)
+	expect(console.log).toHaveBeenNthCalledWith(2, 2)
+	expect(console.log).toHaveBeenNthCalledWith(3, 'Fizz')
+	expect(console.log).toHaveBeenNthCalledWith(4, 4)
+	expect(console.log).toHaveBeenNthCalledWith(5, 'Buzz')
+	expect(console.log).toHaveBeenCalledTimes(num)
 })
 
 
