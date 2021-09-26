571125c610adbbfc66c076881ece2377101edd76
eugeneoei <eugeneoei89@gmail.com>
2021-09-26 19:51:40 +0800

    update test case to check for print statements

--- fizz-buzz/solution.test.js
+++ fizz-buzz/solution.test.js
@@ -1,6 +1,17 @@
 const solution = require('./solution')
 
-it('Should return "1" if number is 1', () => {
-	const answer = solution(1)
-	expect(answer).toEqual(1)
+it('Should print "1" if number is 1', () => {
+	// const answer = solution(1)
+	// expect(answer).toEqual(1)
+	solution(1)
+	console.log = jest.fn()
+	// expect(console.log).toHaveBeenLastCalledWith(1)
+	expect(console.log).toHaveBeenCalledWith(1)
 })
+
+
+
+
+// test('Hello World should return "Hello World!"', () => {
+//   expect(HelloWorld()).toBe('Oh oh ...');
+// });
