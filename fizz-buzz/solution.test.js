de2c9d8d1b83184ade64e07a5715a9acf1fb5d2f
eugeneoei <eugeneoei89@gmail.com>
2021-09-26 19:38:54 +0800

    correct test case where number 1

--- fizz-buzz/solution.test.js
+++ fizz-buzz/solution.test.js
@@ -1,6 +1,6 @@
 const solution = require('./solution')
 
-it('Should return "Fizz" if number is 1', () => {
+it('Should return "1" if number is 1', () => {
 	const answer = solution(1)
-	expect(answer).toEqual('Fizz')
+	expect(answer).toEqual(1)
 })
