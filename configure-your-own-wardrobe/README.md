ad4f2e6ca6b291a5820dd2b465771cfc7236dd22
eugeneoei <eugeneoei89@gmail.com>
2021-10-08 13:32:19 +0800

    update basic combination test case where if available wardrobe sizes is 50, it expected result is [[50,50,50,50,50]]

--- configure-your-own-wardrobe/README.md
+++ configure-your-own-wardrobe/README.md
@@ -3,7 +3,3 @@
 TDD OOP Javascript practice exercise on Configure your own Wardrobe Kata.
 
 For full requirements, refer to [this](https://kata-log.rocks/configure-wardrobe-kata).
-
-# Assumptions
-
-- Wardrobe sizes argument will be a sorted ascending array
--- configure-your-own-wardrobe/solution.test.js
+++ configure-your-own-wardrobe/solution.test.js
@@ -4,7 +4,7 @@ const availableSpace = 250
 
 describe('Combination', () => {
 
-    it('Should return [50, 50, 50, 50, 50]', () => {
+    it('Should return [50, 50, 50, 50, 50] when available wardrobe available sizes is 50', () => {
 
         const availableSizes = [50]
         const expectedResult = [
