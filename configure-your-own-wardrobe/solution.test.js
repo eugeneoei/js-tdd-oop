c0b431dbd2d4f3e77b9d39d557515e8df19ef173
eugeneoei <eugeneoei89@gmail.com>
2021-10-08 13:49:44 +0800

    added test case where if available wardrobe sizes is [50, 75], function should return [[50, 50, 50, 50, 50], [75, 75, 50, 50]]

--- configure-your-own-wardrobe/solution.test.js
+++ configure-your-own-wardrobe/solution.test.js
@@ -4,7 +4,7 @@ const availableSpace = 250
 
 describe('Combination', () => {
 
-    it('Should return [50, 50, 50, 50, 50] when available wardrobe available sizes is 50', () => {
+    it('Should return [50, 50, 50, 50, 50] when available wardrobe available sizes is "[50]"', () => {
 
         const availableSizes = [50]
         const expectedResult = [
@@ -17,10 +17,27 @@ describe('Combination', () => {
 
     })
 
-    it('Should return [] when available wardrobe available sizes is 75', () => {
+    it('Should return [] when available wardrobe available sizes is "[75]"', () => {
 
         const availableSizes = [75]
         const expectedResult = []
+
+        const result = combination(availableSpace, availableSizes)
+
+        expect(result).toEqual(expectedResult)
+
+    })
+
+    it(`Should return [
+        [50, 50, 50, 50, 50],
+        [75, 75, 50, 50]
+    ] when available wardrobe sizes is [50, 75]`, () => {
+
+        const availableSizes = [50, 75]
+        const expectedResult = [
+            [50, 50, 50, 50, 50],
+            [75, 75, 50, 50]
+        ]
 
         const result = combination(availableSpace, availableSizes)
 
