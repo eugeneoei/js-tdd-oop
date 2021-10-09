b9a55c445d6a4355f08ef4d9fffff38e79cddab6
eugeneoei <eugeneoei89@gmail.com>
2021-10-09 15:22:36 +0800

    - refactor test cases to be team method specific - wrote test case for ScoreKeeper class where Team A's score should be 002 when scoreTeamA2 method is called once

--- score-keeper-kata/solution.test.js
+++ score-keeper-kata/solution.test.js
@@ -1,20 +1,27 @@
 const ScoreKeeper = require('./solution')
 
-describe('Scorekeeper Class', () => {
+describe('Team A score method', () => {
 
-    describe('team A score is increased', () => {
+	it('should return 001 when scoreTeamA1 method is called once', () => {
 
-        it('by 1 when scoreTeamA1 method is called', () => {
+		const scoreKeeper = new ScoreKeeper()
+		const expectedResult = '001'
 
-            const scoreKeeper = new ScoreKeeper()
-            const expectedResult = '001'
+		scoreKeeper.scoreTeamA1()
 
-            scoreKeeper.scoreTeamA1()
+		expect(scoreKeeper.getTeamAScore()).toBe(expectedResult)
 
-            expect(scoreKeeper.getTeamAScore()).toBe(expectedResult)
+	})
 
-        })
+	it('should return 002 when scoreTeamA2 method is called once', () => {
 
-    })
+		const scoreKeeper = new ScoreKeeper()
+		const expectedResult = '002'
+
+		scoreKeeper.scoreTeamA2()
+
+		expect(scoreKeeper.getTeamAScore()).toBe(expectedResult)
+
+	})
 
 })
