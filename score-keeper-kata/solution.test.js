ec9911889bacd8959f02348de0efa8004e449c52
eugeneoei <eugeneoei89@gmail.com>
2021-10-09 15:06:45 +0800

    - rename folder to score-keepter-kata - wrote test case for ScoreKeeper class where Team A's score should increase when scoreTeamA1 method is called

---
+++ score-keeper-kata/solution.test.js
@@ -0,0 +1,20 @@
+const ScoreKeeper = require('./solution')
+
+describe('Scorekeeper Class', () => {
+
+    describe('team A score is increased', () => {
+
+        it('by 1 when scoreTeamA1 method is called', () => {
+
+            const scoreKeeper = new ScoreKeeper()
+            const expectedResult = '001'
+
+            scoreKeeper.scoreTeamA1()
+
+            expecte(scoreKeeper.getTeamAScore()).toBe(expectedResult)
+
+        })
+
+    })
+
+})
