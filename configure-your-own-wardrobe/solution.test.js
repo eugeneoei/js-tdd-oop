901b1ce3e06c0949c28f030d313f15d009e9f63f
eugeneoei <eugeneoei89@gmail.com>
2021-10-09 10:56:36 +0800

    added test case where if available wardrobe sizes is [50, 75, 100], function should return [ [50, 50, 50, 50, 50], [50, 50, 50, 100], [50, 50, 75, 75], [50, 100, 100], [75, 75, 100 ]

--- configure-your-own-wardrobe/solution.test.js
+++ configure-your-own-wardrobe/solution.test.js
@@ -4,44 +4,67 @@ const availableSpace = 250
 
 describe('Combination', () => {
 
-	it('Should return [50, 50, 50, 50, 50] when available wardrobe available sizes is "[50]"', () => {
+	// it('Should return [50, 50, 50, 50, 50] when available wardrobe available sizes is "[50]"', () => {
 
-		const availableSizes = [50]
-		const expectedResult = [
-			[50, 50, 50, 50, 50]
-		]
+	// 	const availableSizes = [50]
+	// 	const expectedResult = [
+	// 		[50, 50, 50, 50, 50]
+	// 	]
 
-		const results = combination(availableSpace, availableSizes)
+	// 	const result = combination(availableSpace, availableSizes)
 
-		expect(results).toEqual(expectedResult)
+	// 	expect(result).toEqual(expectedResult)
 
-	})
+	// })
 
-	it('Should return [] when available wardrobe available sizes is "[75]"', () => {
+	// it('Should return [] when available wardrobe available sizes is "[75]"', () => {
 
-		const availableSizes = [75]
-		const expectedResult = []
+	// 	const availableSizes = [75]
+	// 	const expectedResult = []
 
-		const results = combination(availableSpace, availableSizes)
+	// 	const result = combination(availableSpace, availableSizes)
 
-		expect(results).toEqual(expectedResult)
+	// 	expect(result).toEqual(expectedResult)
 
-	})
+	// })
+
+	// it(`Should return [
+	// 	[50, 50, 50, 50, 50],
+	// 	[50, 50, 75, 75]
+	// ] when available wardrobe sizes is [50, 75]`, () => {
+
+	// 	const availableSizes = [50, 75]
+	// 	const expectedResult = [
+	// 		[50, 50, 50, 50, 50],
+	// 		[50, 50, 75, 75]
+	// 	]
+
+	// 	const result = combination(availableSpace, availableSizes)
+
+	// 	expect(result).toEqual(expectedResult)
+
+	// })
 
 	it(`Should return [
 		[50, 50, 50, 50, 50],
-		[50, 50, 75, 75]
-	] when available wardrobe sizes is [50, 75]`, () => {
+		[50, 50, 50, 100],
+		[50, 50, 75, 75],
+		[50, 100, 100],
+		[75, 75, 100]
+	] when available wardrobe sizes is [50, 75, 100]`, () => {
 
-		const availableSizes = [50, 75]
+		const availableSizes = [50, 75, 100]
 		const expectedResult = [
 			[50, 50, 50, 50, 50],
-			[50, 50, 75, 75]
+			[50, 50, 50, 100],
+			[50, 50, 75, 75],
+			[50, 100, 100],
+			[75, 75, 100]
 		]
 
-		const results = combination(availableSpace, availableSizes)
+		const result = combination(availableSpace, availableSizes)
 
-		expect(results).toEqual(expectedResult)
+		expect(result).toEqual(expectedResult)
 
 	})
 
